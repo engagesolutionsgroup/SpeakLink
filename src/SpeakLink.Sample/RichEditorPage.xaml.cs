68ae38389b7c602dfa27aabc185ac652191b54bb
Artem Valieiev <artemvalieiev@outlook.com>
2024-01-29 13:33:07 +0100

    Move format styles to bottom

--- src/SpeakLink.Sample/RichEditorPage.xaml.cs
+++ src/SpeakLink.Sample/RichEditorPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using SpeakLink.RichText;
+﻿using SpeakLink.RichText;
 
 namespace SpeakLink.Sample;
 
@@ -9,62 +8,55 @@ public partial class RichEditorMainPage : ContentPage
     {
         InitializeComponent();
 
-        this.BindingContext = new MentionsViewModel();
+        BindingContext = new MentionsViewModel();
+
+        Loaded += OnLoaded;
+        Unloaded += UnLoaded;
     }
-    
+
+    private void UnLoaded(object? sender, EventArgs e)
+    {
+        Unloaded -= UnLoaded;
+        RichEditor.Focused -= UpdateToolbarPosition;
+        RichEditor.Unfocused -= UpdateToolbarPosition;
+    }
+
+    private void OnLoaded(object? sender, EventArgs e)
+    {
+        Loaded -= OnLoaded;
+        RichEditor.Focused += UpdateToolbarPosition;
+        RichEditor.Unfocused += UpdateToolbarPosition;
+    }
+
+    private void UpdateToolbarPosition(object? sender, FocusEventArgs e)
+    {
+        if (DeviceInfo.Current.Platform == DevicePlatform.iOS)
+        {
+            var additionalScrollFix = 50;
+            TopContent.Margin = new(0, 0, 0, additionalScrollFix);
+            BottomInputLayout.TranslateTo(0, e.IsFocused ? -additionalScrollFix : 0);
+        }
+    }
+
 
     private void HideKeyboard(object? sender, TappedEventArgs e)
     {
-        if (!MentionEditor.IsFocused)
+        if (!RichEditor.IsFocused)
             return;
-        
-        if (MentionEditor.IsSuggestionsPopupVisible
+
+        if (RichEditor.IsSuggestionsPopupVisible
             && e.GetPosition(MentionCollectionView) is { } position
             && position.InsideElement(MentionCollectionView))
         {
             return;
         }
 
-        MentionEditor.Unfocus();
+        RichEditor.Unfocus();
     }
 
     private void UserTappedFromMentionSelector(object? sender, TappedEventArgs e)
     {
         if (e.Parameter is User user)
-            MentionEditor.InsertMention(user.Id.ToString(), user.FullName);
-    }
-
-    private void StyleTapped(object? sender, TappedEventArgs e)
-    {
-        if(sender is VisualElement { BindingContext: IToolbarSpanStyle spanStyle })
-            spanStyle.Toggle();
-    }
-}
-
-public class RichEditorStyleToGlyph : IValueConverter
-{
-    private FormattedString GetFormattedString(string text, FontAttributes fontAttributes, TextDecorations textDec) =>
-        new() { Spans = { new Span { Text = text, FontAttributes = fontAttributes, TextDecorations = textDec } } };
-
-
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        return (RichEditorStyle) value! switch
-        {
-            RichEditorStyle.Bold => GetFormattedString("B", FontAttributes.Bold, TextDecorations.None),
-            RichEditorStyle.Italic => GetFormattedString("I", FontAttributes.Italic, TextDecorations.None),
-            RichEditorStyle.Strikethrough =>
-                GetFormattedString("S", FontAttributes.None, TextDecorations.Strikethrough),
-            RichEditorStyle.Underline => GetFormattedString("S", FontAttributes.None, TextDecorations.Underline),
-            RichEditorStyle.Link => GetFormattedString("L", FontAttributes.None, TextDecorations.None),
-            RichEditorStyle.Subscript => GetFormattedString("Sub", FontAttributes.None, TextDecorations.None),
-            RichEditorStyle.Superscript => GetFormattedString("Sup", FontAttributes.None, TextDecorations.None),
-            _ => throw new ArgumentOutOfRangeException()
-        };
-    }
-
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
+            RichEditor.InsertMention(user.Id.ToString(), user.FullName);
     }
 }
---
+++ src/SpeakLink.Sample/RichEditorStyleToGlyph.cs
@@ -0,0 +1,32 @@
+using System.Globalization;
+using SpeakLink.RichText;
+
+namespace SpeakLink.Sample;
+
+public class RichEditorStyleToGlyph : IValueConverter
+{
+    private FormattedString GetFormattedString(string text, FontAttributes fontAttributes, TextDecorations textDec) =>
+        new() { Spans = { new Span { Text = text, FontAttributes = fontAttributes, TextDecorations = textDec } } };
+
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return (RichEditorStyle) value! switch
+        {
+            RichEditorStyle.Bold => GetFormattedString("B", FontAttributes.Bold, TextDecorations.None),
+            RichEditorStyle.Italic => GetFormattedString("I", FontAttributes.Italic, TextDecorations.None),
+            RichEditorStyle.Strikethrough =>
+                GetFormattedString("S", FontAttributes.None, TextDecorations.Strikethrough),
+            RichEditorStyle.Underline => GetFormattedString("S", FontAttributes.None, TextDecorations.Underline),
+            RichEditorStyle.Link => GetFormattedString("L", FontAttributes.None, TextDecorations.None),
+            RichEditorStyle.Subscript => GetFormattedString("Sub", FontAttributes.None, TextDecorations.None),
+            RichEditorStyle.Superscript => GetFormattedString("Sup", FontAttributes.None, TextDecorations.None),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
